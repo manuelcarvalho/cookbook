45dbb0802646c269c61fca7e51fcb41eb8f72923
manuel carvalho <manuelcarvalho@gmail.com>
2013-05-31 21:11:42 +0100

    Apagar Receita

--- src/main/java/pt/ulht/es/cookbook/controller/NotFoundController.java
+++ src/main/java/pt/ulht/es/cookbook/controller/NotFoundController.java
@@ -1,6 +1,5 @@
 package pt.ulht.es.cookbook.controller;
 
-//teste
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
